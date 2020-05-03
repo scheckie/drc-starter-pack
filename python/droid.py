881e8efd2d456f8da5bbfe2c5f120d3a3eb1c8ba
Callum Hays <callumjhays@gmail.com>
2020-05-03 14:16:21 +1000

    mjpeg over b64-image-raws for speed

--- python/droid.py
+++ python/droid.py
@@ -1,5 +1,8 @@
+"starter drc droid example in python"
+
+# use roslibpy (ROS over websockets) because the python packages are only available in linux.
 from roslibpy import Ros, Topic, Message
-import time
+import cv2
 
 # change this to the IP shown in your VM
 GAZEBO_VM_IP = '192.168.98.129'
@@ -7,22 +10,22 @@ GAZEBO_VM_IP = '192.168.98.129'
 client = Ros(host=GAZEBO_VM_IP, port=9090)
 client.run()
 
-camera = Topic(client, '/droid/camera/image_raw', 'sensor_msgs/Image')
-
-def image_callback(msg):
-    # TODO: parse image message data and display using matplotlib
-    
-    drive.publish(Message({
-        'linear': { 'x': 0.1 },
-        'angular': { 'z': 0.5 }
-    }))
-
-camera.subscribe(image_callback)
+camera = cv2.VideoCapture(f"http://{GAZEBO_VM_IP}:8080/stream?topic=/droid/camera/image_raw")
 
 drive = Topic(client, '/cmd_vel', 'geometry_msgs/Twist')
+drive.advertise()
 
-while client.is_connected:
-    time.sleep(1)
+drive.publish(Message({
+    'linear': {'x': 0.1},
+    'angular': {'z': 0.5}
+}))
 
-camera.unsubscribe()
-client.terminate()
+
+try:
+    while client.is_connected:
+        _, image = camera.read()
+
+        cv2.imshow('RobotCamera', image)
+        cv2.waitKey(1)
+finally:
+    cv2.destroyAllWindows()
