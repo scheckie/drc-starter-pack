15eba72e0a74de4f0fb7a4d72be00790400fa210
Callum Hays <callumjhays@gmail.com>
2020-05-03 14:33:41 +1000

    embellish comment

--- python/droid.py
+++ python/droid.py
@@ -11,7 +11,7 @@ client = Ros(host=GAZEBO_VM_IP, port=9090)
 client.run()
 
 # consume frames from the mjpeg stream because rosbridge (websocket) encodes the binary
-# images as b64, causing a huge performance hit.
+# images as b64, because it wraps all messages in JSON, causing a huge performance hit.
 camera = cv2.VideoCapture(f"http://{GAZEBO_VM_IP}:8080/stream?topic=/droid/camera/image_raw")
 
 drive = Topic(client, '/cmd_vel', 'geometry_msgs/Twist')
